28aa807ca2498dcd43e414f2b445a9baf04cb977
Takahiro Hirata <ta.hirata@opt.ne.jp>
2020-06-30 01:39:06 +0900

    add: photo resolver

--- photo-share-api/index.js
+++ photo-share-api/index.js
@@ -1,22 +1,53 @@
-const { ApolloServer } = require(`apollo-server`)
+const { ApolloServer } = require('apollo-server');
 
 const typeDefs = `
+  type Photo {
+    id: ID!
+    url: String!
+    name: String!
+    description: String
+  }
+
   type Query {
     totalPhotos: Int!
+    allPhotos: [Photo!]!
   }
-`
+
+  type Mutation {
+    postPhoto(name: String! description: String): Photo!
+  }
+
+`;
+
+let id = 0;
+const photos = [];
 
 const resolvers = {
   Query: {
-    totalPhotos: () => 42
-  }
-}
+    totalPhotos: () => photos.length,
+    allPhotos: () => photos,
+  },
+  Mutation: {
+    postPhoto(parent, args) {
+      const newPhoto = {
+        id: id += 1,
+        ...args,
+      };
+
+      photos.push(newPhoto);
+      return newPhoto;
+    },
+  },
+  Photo: {
+    url: (parent) => `http://yoursite.com/img/${parent.id}.jpg`,
+  },
+};
 
 const server = new ApolloServer({
   typeDefs,
-  resolvers
-})
+  resolvers,
+});
 
 server
   .listen()
-  .then(({url}) => console.log(`GraphWL Service running on ${url}`))
+  .then(({ url }) => console.log(`GraphWL Service running on ${url}`));
