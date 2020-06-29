8476534917d76bdd04ab8eff17b13508804e24ec
Takahiro Hirata <ta.hirata@opt.ne.jp>
2020-06-30 02:18:38 +0900

    1対多のリレーション

--- photo-share-api/index.js
+++ photo-share-api/index.js
@@ -15,6 +15,14 @@ const typeDefs = `
     name: String!
     description: String
     category: PhotoCategory!
+    postedBy: User!
+  }
+
+  type User {
+    githubLogin: ID!
+    name: String
+    avatar: String
+    postedPhotos: [Photo!]!
   }
 
   input PostPhotoInput {
@@ -35,7 +43,36 @@ const typeDefs = `
 `;
 
 let id = 0;
-const photos = [];
+
+const users = [
+  { githubLogin: 'mHattrup', name: 'Mike Hattrup' },
+  { githubLogin: 'gPlake', name: 'Glen Plake' },
+  { githubLogin: 'sSchmidt', name: 'Scot Schmidt' },
+];
+
+const photos = [
+  {
+    id: 1,
+    name: 'photo 1',
+    description: 'description 1',
+    category: 'ACTION',
+    githubUser: 'gPlake',
+  },
+  {
+    id: 2,
+    name: 'photo 2',
+    description: 'description 2',
+    category: 'SELFIE',
+    githubUser: 'sSchmidt',
+  },
+  {
+    id: 3,
+    name: 'photo 3',
+    description: 'description 3',
+    category: 'LANDSCAPE',
+    githubUser: 'sSchmidt',
+  },
+];
 
 const resolvers = {
   Query: {
@@ -55,6 +92,10 @@ const resolvers = {
   },
   Photo: {
     url: (parent) => `http://yoursite.com/img/${parent.id}.jpg`,
+    postedBy: (parent) => users.find((user) => user.githubLogin === parent.githubUser),
+  },
+  User: {
+    postedPhotos: (parent) => photos.filter((photo) => photo.githubUser === parent.githubLogin),
   },
 };
 
